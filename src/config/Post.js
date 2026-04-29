7c00f956dd759c6e9b9f3dae1fa3d292ddafdd1a
Gracee001-M <gracemacharia031@gmail.com>
2026-04-29 18:24:10 +0300

    Add User-Post Relationship

--- src/config/Post.js
+++ src/config/Post.js
@@ -54,3 +54,20 @@ postSchema.set('toJSON', { virtuals: true });
 module.exports = mongoose.model('Post', postSchema);
 
 
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tags: [{ type: String, trim: true }],
+    likes: { type: Number, default: 0 },
+    published: { type: Boolean, default: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', postSchema);
+
